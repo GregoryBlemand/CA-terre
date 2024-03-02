c4af409830655b182134637501ccffc7cf1c4978
GregB <gregoryvalence@gmail.com>
2024-03-02 17:33:40 +0100

    terre-5

---
+++ 05_pair_impair.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Consigne :
+ *
+ * Créez un programme qui permet de déterminer si l’argument donné est un entier pair ou impair..
+ *
+ * Exemples d’utilisation :
+ * $> ruby exo.rb 2
+ * pair
+ *
+ * $> ruby exo.rb 5
+ * impair
+ *
+ * $> ruby exo.rb Bonjour
+ * Tu ne me la mettras pas à l’envers.
+ *
+ * $> ruby exo.rb
+ * Tu ne me la mettras pas à l’envers.
+ *
+ * Attention : gérez aussi les entiers négatifs.
+ *
+ * Fonctions interdites:
+ * -En Ruby: even? et odd?
+ *
+ */
+
+if ($argc === 1) {
+    print "Tu ne me la mettras pas à l’envers.\n";
+    exit;
+}
+
+if ($argc > 2) {
+    print "Trop d'arguments... un seul suffit\n";
+    exit;
+}
+
+$argument = $argv[1];
+if (preg_match('/\D/', $argument) && !str_contains($argument, '-')) {
+    print "Tu ne me la mettras pas à l’envers.\n";
+    exit;
+}
+
+print empty($argument % 2) ? "pair\n" : "impair\n";
--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
 Epreuve de la terre - coding accelerator
 
- - Alphabet dans l'ordre
- - Nom de fichier
- - Afficheur d'argument
- - Alphabet à partir de
- - Pair ou impair
- - Division
- - Inversion d'une chaine
- - Taille de chaine
- - Puissance du nombre
- - Racine carré du nombre
- - Nombre premier
- - Horloge 24 to 12
- - Trouver la suisse
- - Trié ou non
- - Terre OK
+ 1) Alphabet dans l'ordre
+ 2) Nom de fichier
+ 3) Afficheur d'argument
+ 4) Alphabet à partir de
+ 5) Pair ou impair
+ 6) Division
+ 7) Inversion d'une chaine
+ 8) Taille de chaine
+ 9) Puissance du nombre
+ 10) Racine carré du nombre
+ 11) Nombre premier
+ 12) Horloge 24 to 12
+ 13) Trouver la suisse 
+ 14) Trié ou non
+ 15) Terre OK
