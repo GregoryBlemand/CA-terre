5f169aa24a63dc119c03b6585822d42036764c90
GregB <gregoryvalence@gmail.com>
2024-03-07 09:12:47 +0100

    terre-16

---
+++ 16_terre_ok.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Consigne :
+ *
+ * Créez un programme qui célèbre votre victoire.
+ *
+ * Exemples d’utilisation :
+ * $> ruby exo.rb
+ * J’ai terminé l’Épreuve de la Terre et c’était [].
+ * $>
+ *
+ * Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+ *
+ */
+
+print "J’ai terminé l’Épreuve de la Terre et c’était facile sauf sur les derniers exercices.\n";
